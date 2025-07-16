6f1390008149c190ffca7bdc3c7474243997b024
Porco Rosso <porcorossobaojie@gmail.com>
2025-07-16 14:56:00 +0800

    修改代码格式

--- data_source/joinquant/ann_dt_table/main.py
+++ data_source/joinquant/ann_dt_table/main.py
@@ -1,41 +1,49 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 15 14:21:35 2025
+Created on Tue Jul 15 20:23:04 2025
 
 @author: Porco Rosso
 
 """
+from typing import Any, Literal
 
-from data_source.joinquant.meta.main import main as meta
+import jqdatasdk as jq
+import pandas as pd
 
 from data_source.joinquant.config import ANN_DT_TABLES as config
-from typing import Literal, Any
+from data_source.joinquant.meta.main import main as meta
+
 
 class main(meta):
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-    
-    def daily(self, if_exists: Literal['append', 'replace'] = 'append'):
+
+    def pipeline(self, **kwargs: Any) -> pd.DataFrame:
+        df = super().pipeline(**kwargs)
+        return df
+
+    def daily(self, if_exists: Literal['append', 'replace'] = 'append') -> None:
         if if_exists == 'replace':
             self.drop_table()
-        
+
         if not self.table_exist():
             self.create_table()
-        
-        id_key = self.__find_max_of_exist_table__(self.id_key)
-        df = self.pipeline(id_key=id_key)
-        self.write(df, log=True)
-        while len(df):
-            id_key = df[self.id_key].max()
-            df = self.pipeline(id_key=id_key)
+
+        id_key = self.__find_max_of_exist_table__(self.ann_dt)
+        days = self._trade_days.copy()
+        days = days[days > id_key]
+
+        for i in days:
+            if jq.get_query_count()['spare'] <= 5000000:
+                break
+            df = self.pipeline(date=f'{i.date()}')
+            print(i)
             self.write(df, log=True)
+
 
 '''
 test:
-self =main(**config.asharebalancesheet)
-self.daily()
 
+self = main(**config.asharebalancesheet)
+self.daily()
 '''
-    
-    
-    
--- data_source/joinquant/trade_dt_table/main.py
+++ data_source/joinquant/trade_dt_table/main.py
@@ -5,21 +5,22 @@ Created on Tue Jul 15 20:23:04 2025
 @author: Porco Rosso
 
 """
-import pandas as pd
+from typing import Any, Literal
 
 import jqdatasdk as jq
-from data_source.joinquant.meta.main import main as meta
+import pandas as pd
 
 from data_source.joinquant.config import TRADE_DT_TABLES as config
-from typing import Literal, Any
+from data_source.joinquant.meta.main import main as meta
+
 
 class main(meta):
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        
-    def pipeline(self, **kwargs):
+
+    def pipeline(self, **kwargs: Any) -> pd.DataFrame:
         df = super().pipeline(**kwargs)
-        
+
         # special fix:
         # 1. construct returns of portfolio by calculated with close price(first use adj price if have)
         # 2. adjust weight total from 100 to 1
@@ -28,39 +29,39 @@ class main(meta):
         if pct_key in self.columns.keys():
             try:
                 df[pct_key] = df['S_DQ_CLOSE_ADJ'] / df['S_DQ_PRECLOSE_ADJ'] - 1
-            except:
+            except Exception:
                 df[pct_key] = df['S_DQ_CLOSE'] / df['S_DQ_PRECLOSE'] - 1
         if weight_key in self.columns.keys():
             df[weight_key] = df[weight_key] / 100
-            
+
         df = df[df.notnull().sum(axis=1) > df.shape[1] * 0.6]
         return df
-    
-    def daily(self, if_exists: Literal['append', 'replace'] = 'append'):
-        if self.table == 'asharelisting': # this table inform the on list time for each stock, which will replace every day
-            if_exists == 'replace'
-            
+
+    def daily(self, if_exists: Literal['append', 'replace'] = 'append') -> None:
+        if self.table == 'asharelisting':  # this table inform the on list time for each stock, which will replace every day
+            if_exists = 'replace'
+
         if if_exists == 'replace':
             self.drop_table()
-        
+
         if not self.table_exist():
             self.create_table()
-        
+
         id_key = self.__find_max_of_exist_table__(self.trade_dt)
-        if self.table == 'ashareconcept': # this table inform the ’题材‘ and '概念' for which not have data at 2010, the earlest data appeared at 2015
+        if self.table == 'ashareconcept':  # this table inform the ’题材‘ and '概念' for which not have data at 2010, the earlest data appeared at 2015
             id_key = max(pd.to_datetime('2015-01-01 15:00'), id_key)
         days = self._trade_days.copy()
         days = days[days > id_key]
-        if  (self.table == 'asharelisting') and len(days):
+        if self.table == 'asharelisting' and len(days):
             days = days[-1:]
-            
+
         for i in days:
             if jq.get_query_count()['spare'] <= 5000000:
                 break
             df = self.pipeline(date=f'{i.date()}')
             print(i)
             self.write(df, log=True)
-            
+
 
 '''
 test:
@@ -68,7 +69,3 @@ test:
 self = main(**config.asharelisting)
 self.daily()
 '''
-
-
-
-
